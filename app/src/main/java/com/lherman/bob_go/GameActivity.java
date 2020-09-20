fda247c75d95ef886d5c9d571cd246d77684c253
L1ndseyHerman <lherman21@jcu.edu>
2020-09-19 20:35:03 -0400

    GameActivity recognises tap gesture anywhere on screen.

--- app/src/main/java/com/lherman/bob_go/GameActivity.java
+++ app/src/main/java/com/lherman/bob_go/GameActivity.java
@@ -3,12 +3,28 @@ package com.lherman.bob_go;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    //  This method recognises a tap gesture anywhere on the current screen (Activity).
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        //  Where tf is the console?! Doesn't show anywhere....
+        //System.out.println("A tap!");
+
+        TextView clickText = (TextView)findViewById(R.id.clickText);
+        clickText.setText("You clicked the screen!");
+        return true;
     }
 }
