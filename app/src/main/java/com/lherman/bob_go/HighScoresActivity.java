dbccce049c2ea9e5a7e60d082d87389644f4d2c6
L1ndseyHerman <lherman21@jcu.edu>
2020-09-23 21:16:37 -0400

    600x800 is not a square, facepalm! Resized bob and square, made winsquare and hater, and tested them across all 3 Activities.

--- app/src/main/java/com/lherman/bob_go/HighScoresActivity.java
+++ app/src/main/java/com/lherman/bob_go/HighScoresActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HighScoresActivity extends AppCompatActivity
 {
@@ -28,9 +29,17 @@ public class HighScoresActivity extends AppCompatActivity
         int width = size.x;
         int height = size.y;
 
+        int halfWidth = size.x/2;
+        int halfHeight = size.y/2;
+
         //  Is this showing up anywhere?
         //Log.e("Width", "" + width);
         //Log.e("height", "" + height);
+        String stringWidth = Integer.toString(width);
+        String stringHeight = Integer.toString(height);
+
+        String halfStringWidth = Integer.toString(halfWidth);
+        String halfStringHeight = Integer.toString(halfHeight);
 
         //  Resizing square based on screen size.
         ImageView squareLeft = findViewById(R.id.squareLeft);
@@ -40,5 +49,13 @@ public class HighScoresActivity extends AppCompatActivity
         squareRight.getLayoutParams().height = height/2;
         squareRight.getLayoutParams().width = width/2;
 
+        TextView theText = findViewById(R.id.textViewTest);
+        int x = (int) squareRight.getX();
+        String xString = Integer.toString(x);
+
+        //theText.setText(theText.getText() + stringWidth + "," + stringHeight + "," + xString + "," + squareRight.getY() + "," + squareRight.getWidth() + "," + squareRight.getHeight());
+        theText.setText("squareRightLeft = " + halfStringWidth + " squareRightRight = " + stringWidth);
     }
+
+
 }
