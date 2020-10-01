d27cafdeb764ca56aabfb24466f4b2c6c6b6873a
L1ndseyHerman <lherman21@jcu.edu>
2020-09-30 20:27:44 -0400

    Timer or Animation? Tested both here.

--- app/src/main/java/com/lherman/bob_go/GameActivity.java
+++ app/src/main/java/com/lherman/bob_go/GameActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity
 {
     boolean jumpingUp = false;
     boolean fallingDown = false;
+    int clickCount = 0;
     //TextView clickText = findViewById(R.id.clickText);
     //ImageView bobImage = findViewById(R.id.bobImage);
     //private TranslateAnimation movingBob;
@@ -36,6 +42,17 @@ public class GameActivity extends AppCompatActivity
         moveDownwards.setFillAfter(true);
         moveDownwards.setRepeatCount(-1);
         findViewById(R.id.bobImage).startAnimation(moveDownwards);*/
+        //final TextView clickText = findViewById(R.id.clickText);
+        Timer timer = new Timer();
+        TimerTask t = new TimerTask() {
+            @Override
+            public void run() {
+
+                System.out.println("Woo hoo!");
+                //clickText.setText();
+            }
+        };
+        timer.scheduleAtFixedRate(t,1000,1000);
     }
 
     //  This method recognises a tap gesture anywhere on the current screen (Activity).
@@ -48,24 +65,63 @@ public class GameActivity extends AppCompatActivity
         //clickText.setText("You clicked the screen!");
 
         //ImageView bobImage = findViewById(R.id.bobImage);
-        TranslateAnimation movingBob;
-
+        TranslateAnimation jumping;
+        TranslateAnimation falling;
+        clickCount++;
+        jumping = new TranslateAnimation(0, 0, 0, -161);
 
         //bobImage.setX(bobImage.getX() + 30);
-        if (jumpingUp == false && fallingDown == false) {
+        if (jumpingUp == false && fallingDown == false)
+        {
+        //if (clickCount == 1)
             jumpingUp = true;
             clickText.setText("Jumping up.");
 
-            movingBob = new TranslateAnimation(0, 0, 0, 1000);
-            movingBob.setDuration(3000);
-            movingBob.setFillAfter(true);
-            movingBob.setRepeatCount(-1);
-            findViewById(R.id.bobImage).startAnimation(movingBob);
+
+            jumping.setDuration(1000);
+            jumping.setFillAfter(true);
+            jumping.setInterpolator(new DecelerateInterpolator(2.f));
+            //new DecelerateInterpolator(2);
+            //movingBob.setRepeatCount(-1);
+            findViewById(R.id.bobImage).startAnimation(jumping);
+
             //bobImage.setX(bobImage.getX() + 30);
+            //jumpingUp = false;
+            //fallingDown = true;
+        }
+        //else if (jumpingUp==false && fallingDown==true)
+        /*else if (clickCount == 2)
+        {
+            clickText.setText("Falling down.");
+
+            falling = new TranslateAnimation(0, 0, 0, 161);
+            falling.setDuration(1000);
+            falling.setFillAfter(true);
+            falling.setInterpolator(new AccelerateInterpolator(2.f));
+            //new DecelerateInterpolator(2);
+            //movingBob.setRepeatCount(-1);
+            findViewById(R.id.bobImage).startAnimation(falling);
+            //bobImage.setX(bobImage.getX() + 30);
+
+        }*/
+        else if (jumping.hasEnded()) {
+            fallingDown = true;
+            jumpingUp = false;
+            clickText.setText("Falling down");
+
+            falling = new TranslateAnimation(0, 0, 0, 161);
+            falling.setDuration(1000);
+            falling.setFillAfter(true);
+            falling.setInterpolator(new AccelerateInterpolator(2.f));
+            //new DecelerateInterpolator(2);
+            //movingBob.setRepeatCount(-1);
+            findViewById(R.id.bobImage).startAnimation(falling);
+            //bobImage.setX(bobImage.getX() + 30);)
         }
         else
         {
-            clickText.setText("Falling down.");
+            clickText.setText("Extra click.");
+            //fallingDown = false;
         }
 
         //  Below just has to be there for some reason:
