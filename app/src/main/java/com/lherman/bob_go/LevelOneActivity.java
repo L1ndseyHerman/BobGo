ca2617577134f6e325cd7005d8494bdcaa3e76e3
L1ndseyHerman <lherman21@jcu.edu>
2020-10-13 10:20:06 -0400

    LevelOneActivity basic grid

--- app/src/main/java/com/lherman/bob_go/LevelOneActivity.java
+++ app/src/main/java/com/lherman/bob_go/LevelOneActivity.java
@@ -2,15 +2,135 @@ package com.lherman.bob_go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class LevelOneActivity extends AppCompatActivity
 {
+    private ImageView[][] daGrid = new ImageView[12][6];
+    private ImageView bob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+
+        //  Screen size stuff:
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
+        //  The number in the left brackets is the x and the right is the y,
+        //  so like "theSquares[0][3]" is all the way to the left, but three squares down.
+        daGrid[0][0] = findViewById(R.id.grid0x0);
+        daGrid[0][1] = findViewById(R.id.grid0x1);
+        daGrid[0][2] = findViewById(R.id.grid0x2);
+        daGrid[0][3] = findViewById(R.id.grid0x3);
+        daGrid[0][4] = findViewById(R.id.grid0x4);
+        daGrid[0][5] = findViewById(R.id.grid0x5);
+
+        daGrid[1][0] = findViewById(R.id.grid1x0);
+        daGrid[1][1] = findViewById(R.id.grid1x1);
+        daGrid[1][2] = findViewById(R.id.grid1x2);
+        daGrid[1][3] = findViewById(R.id.grid1x3);
+        daGrid[1][4] = findViewById(R.id.grid1x4);
+        daGrid[1][5] = findViewById(R.id.grid1x5);
+
+        daGrid[2][0] = findViewById(R.id.grid2x0);
+        daGrid[2][1] = findViewById(R.id.grid2x1);
+        daGrid[2][2] = findViewById(R.id.grid2x2);
+        daGrid[2][3] = findViewById(R.id.grid2x3);
+        daGrid[2][4] = findViewById(R.id.grid2x4);
+        daGrid[2][5] = findViewById(R.id.grid2x5);
+
+        daGrid[3][0] = findViewById(R.id.grid3x0);
+        daGrid[3][1] = findViewById(R.id.grid3x1);
+        daGrid[3][2] = findViewById(R.id.grid3x2);
+        daGrid[3][3] = findViewById(R.id.grid3x3);
+        daGrid[3][4] = findViewById(R.id.grid3x4);
+        daGrid[3][5] = findViewById(R.id.grid3x5);
+
+        daGrid[4][0] = findViewById(R.id.grid4x0);
+        daGrid[4][1] = findViewById(R.id.grid4x1);
+        daGrid[4][2] = findViewById(R.id.grid4x2);
+        daGrid[4][3] = findViewById(R.id.grid4x3);
+        daGrid[4][4] = findViewById(R.id.grid4x4);
+        daGrid[4][5] = findViewById(R.id.grid4x5);
+
+        daGrid[5][0] = findViewById(R.id.grid5x0);
+        daGrid[5][1] = findViewById(R.id.grid5x1);
+        daGrid[5][2] = findViewById(R.id.grid5x2);
+        daGrid[5][3] = findViewById(R.id.grid5x3);
+        daGrid[5][4] = findViewById(R.id.grid5x4);
+        daGrid[5][5] = findViewById(R.id.grid5x5);
+
+        daGrid[6][0] = findViewById(R.id.grid6x0);
+        daGrid[6][1] = findViewById(R.id.grid6x1);
+        daGrid[6][2] = findViewById(R.id.grid6x2);
+        daGrid[6][3] = findViewById(R.id.grid6x3);
+        daGrid[6][4] = findViewById(R.id.grid6x4);
+        daGrid[6][5] = findViewById(R.id.grid6x5);
+
+        daGrid[7][0] = findViewById(R.id.grid7x0);
+        daGrid[7][1] = findViewById(R.id.grid7x1);
+        daGrid[7][2] = findViewById(R.id.grid7x2);
+        daGrid[7][3] = findViewById(R.id.grid7x3);
+        daGrid[7][4] = findViewById(R.id.grid7x4);
+        daGrid[7][5] = findViewById(R.id.grid7x5);
+
+        daGrid[8][0] = findViewById(R.id.grid8x0);
+        daGrid[8][1] = findViewById(R.id.grid8x1);
+        daGrid[8][2] = findViewById(R.id.grid8x2);
+        daGrid[8][3] = findViewById(R.id.grid8x3);
+        daGrid[8][4] = findViewById(R.id.grid8x4);
+        daGrid[8][5] = findViewById(R.id.grid8x5);
+
+        daGrid[9][0] = findViewById(R.id.grid9x0);
+        daGrid[9][1] = findViewById(R.id.grid9x1);
+        daGrid[9][2] = findViewById(R.id.grid9x2);
+        daGrid[9][3] = findViewById(R.id.grid9x3);
+        daGrid[9][4] = findViewById(R.id.grid9x4);
+        daGrid[9][5] = findViewById(R.id.grid9x5);
+
+        daGrid[10][0] = findViewById(R.id.grid10x0);
+        daGrid[10][1] = findViewById(R.id.grid10x1);
+        daGrid[10][2] = findViewById(R.id.grid10x2);
+        daGrid[10][3] = findViewById(R.id.grid10x3);
+        daGrid[10][4] = findViewById(R.id.grid10x4);
+        daGrid[10][5] = findViewById(R.id.grid10x5);
+
+        daGrid[11][0] = findViewById(R.id.grid11x0);
+        daGrid[11][1] = findViewById(R.id.grid11x1);
+        daGrid[11][2] = findViewById(R.id.grid11x2);
+        daGrid[11][3] = findViewById(R.id.grid11x3);
+        daGrid[11][4] = findViewById(R.id.grid11x4);
+        daGrid[11][5] = findViewById(R.id.grid11x5);
+
+
+        for (int index=0; index<daGrid.length; index++)
+        {
+            for (int index2=0; index2<daGrid[index].length; index2++)
+            {
+                daGrid[index][index2].getLayoutParams().height = height / 7;
+                daGrid[index][index2].getLayoutParams().width = width / 12;
+                daGrid[index][index2].setX(index*width/12);
+                //daGrid[index][index2].setX((width)+(index*width/12));
+                daGrid[index][index2].setY((height / 14) + (index2 * height / 7));
+            }
+        }
+
+
+        bob = findViewById(R.id.bob);
+        bob.setX(0);
+        bob.setY(11*height/14);
+        bob.getLayoutParams().height = height/7;
+        bob.getLayoutParams().width = width/12;
+
+
     }
 }
