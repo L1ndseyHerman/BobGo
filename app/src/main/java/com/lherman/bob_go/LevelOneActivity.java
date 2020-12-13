e5b09b25a999354f900aa3687c8a3cd61b9c56a9
L1ndseyHerman <lherman21@jcu.edu>
2020-12-13 18:22:38 -0500

    2Squares = Bob jumps twice as fast :( + deleted LevelOneActivity old code.

--- app/src/main/java/com/lherman/bob_go/LevelOneActivity.java
+++ app/src/main/java/com/lherman/bob_go/LevelOneActivity.java
@@ -14,12 +14,8 @@ import java.util.TimerTask;
 
 public class LevelOneActivity extends AppCompatActivity
 {
-    //  The grid of SquareObstacles and BlankGridSpaces.
-    //private ImageView[][] daGrid = new ImageView[13][6];
-    //  Time to make them objects!
-    private GridImageThing[][] daGrid = new GridImageThing[13][6];
-
-
+    //  The grid of SquareObstacles and BlankGridSpaces
+    private GridImageThing[][] daGrid = new GridImageThing[14][6];
     //  The one and only Image of Bob! :D
     private ImageView bobImage;
     //  The amount that everything in daGrid and the enemies move every timer call.
@@ -36,11 +32,8 @@ public class LevelOneActivity extends AppCompatActivity
     //  Will be the width and height of the user's phone/tablet screen, decided at runtime.
     int width;
     int height;
-
     //  The one and only object of Bob! :D
     Bob bob;
-    //  Only one for now....
-    //SquareObstacle onlySquare;
 
     //  Android Studio's Main Method:
     @Override
@@ -61,107 +54,13 @@ public class LevelOneActivity extends AppCompatActivity
         //  Same proportion for y-direction, 7*14=98
         yBobJump = height/98;
 
-        //  The number in the left brackets is the x and the right is the y,
-        //  so like "theSquares[0][3]" is all the way to the left, but three squares down.
-        /*
-        daGrid[0][0] = findViewById(R.id.grid0x0);
-        daGrid[0][1] = findViewById(R.id.grid0x1);
-        daGrid[0][2] = findViewById(R.id.grid0x2);
-        daGrid[0][3] = findViewById(R.id.grid0x3);
-        daGrid[0][4] = findViewById(R.id.grid0x4);
-        daGrid[0][5] = findViewById(R.id.grid0x5);
-
-        daGrid[1][0] = findViewById(R.id.grid1x0);
-        daGrid[1][1] = findViewById(R.id.grid1x1);
-        daGrid[1][2] = findViewById(R.id.grid1x2);
-        daGrid[1][3] = findViewById(R.id.grid1x3);
-        daGrid[1][4] = findViewById(R.id.grid1x4);
-        daGrid[1][5] = findViewById(R.id.grid1x5);
-
-        daGrid[2][0] = findViewById(R.id.grid2x0);
-        daGrid[2][1] = findViewById(R.id.grid2x1);
-        daGrid[2][2] = findViewById(R.id.grid2x2);
-        daGrid[2][3] = findViewById(R.id.grid2x3);
-        daGrid[2][4] = findViewById(R.id.grid2x4);
-        daGrid[2][5] = findViewById(R.id.grid2x5);
-
-        daGrid[3][0] = findViewById(R.id.grid3x0);
-        daGrid[3][1] = findViewById(R.id.grid3x1);
-        daGrid[3][2] = findViewById(R.id.grid3x2);
-        daGrid[3][3] = findViewById(R.id.grid3x3);
-        daGrid[3][4] = findViewById(R.id.grid3x4);
-        daGrid[3][5] = findViewById(R.id.grid3x5);
-
-        daGrid[4][0] = findViewById(R.id.grid4x0);
-        daGrid[4][1] = findViewById(R.id.grid4x1);
-        daGrid[4][2] = findViewById(R.id.grid4x2);
-        daGrid[4][3] = findViewById(R.id.grid4x3);
-        daGrid[4][4] = findViewById(R.id.grid4x4);
-        daGrid[4][5] = findViewById(R.id.grid4x5);
-
-        daGrid[5][0] = findViewById(R.id.grid5x0);
-        daGrid[5][1] = findViewById(R.id.grid5x1);
-        daGrid[5][2] = findViewById(R.id.grid5x2);
-        daGrid[5][3] = findViewById(R.id.grid5x3);
-        daGrid[5][4] = findViewById(R.id.grid5x4);
-        daGrid[5][5] = findViewById(R.id.grid5x5);
-
-        daGrid[6][0] = findViewById(R.id.grid6x0);
-        daGrid[6][1] = findViewById(R.id.grid6x1);
-        daGrid[6][2] = findViewById(R.id.grid6x2);
-        daGrid[6][3] = findViewById(R.id.grid6x3);
-        daGrid[6][4] = findViewById(R.id.grid6x4);
-        daGrid[6][5] = findViewById(R.id.grid6x5);
-
-        daGrid[7][0] = findViewById(R.id.grid7x0);
-        daGrid[7][1] = findViewById(R.id.grid7x1);
-        daGrid[7][2] = findViewById(R.id.grid7x2);
-        daGrid[7][3] = findViewById(R.id.grid7x3);
-        daGrid[7][4] = findViewById(R.id.grid7x4);
-        daGrid[7][5] = findViewById(R.id.grid7x5);
-
-        daGrid[8][0] = findViewById(R.id.grid8x0);
-        daGrid[8][1] = findViewById(R.id.grid8x1);
-        daGrid[8][2] = findViewById(R.id.grid8x2);
-        daGrid[8][3] = findViewById(R.id.grid8x3);
-        daGrid[8][4] = findViewById(R.id.grid8x4);
-        daGrid[8][5] = findViewById(R.id.grid8x5);
-
-        daGrid[9][0] = findViewById(R.id.grid9x0);
-        daGrid[9][1] = findViewById(R.id.grid9x1);
-        daGrid[9][2] = findViewById(R.id.grid9x2);
-        daGrid[9][3] = findViewById(R.id.grid9x3);
-        daGrid[9][4] = findViewById(R.id.grid9x4);
-        daGrid[9][5] = findViewById(R.id.grid9x5);
-
-        daGrid[10][0] = findViewById(R.id.grid10x0);
-        daGrid[10][1] = findViewById(R.id.grid10x1);
-        daGrid[10][2] = findViewById(R.id.grid10x2);
-        daGrid[10][3] = findViewById(R.id.grid10x3);
-        daGrid[10][4] = findViewById(R.id.grid10x4);
-        daGrid[10][5] = findViewById(R.id.grid10x5);
-
-        daGrid[11][0] = findViewById(R.id.grid11x0);
-        daGrid[11][1] = findViewById(R.id.grid11x1);
-        daGrid[11][2] = findViewById(R.id.grid11x2);
-        daGrid[11][3] = findViewById(R.id.grid11x3);
-        daGrid[11][4] = findViewById(R.id.grid11x4);
-        daGrid[11][5] = findViewById(R.id.grid11x5);
-
-        daGrid[12][0] = findViewById(R.id.grid12x0);
-        daGrid[12][1] = findViewById(R.id.grid12x1);
-        daGrid[12][2] = findViewById(R.id.grid12x2);
-        daGrid[12][3] = findViewById(R.id.grid12x3);
-        daGrid[12][4] = findViewById(R.id.grid12x4);
-        daGrid[12][5] = findViewById(R.id.grid12x5);
-        */
-
         //  Stuff for the one and only Bob
         //  REALLY DON'T FORGET TO PUT IT ABOVE THE SQUAREOBSTACLES THAT REFERENCE IT!!
         bobImage = findViewById(R.id.bob);
         bob = new Bob(bobImage, width, height);
 
-
+        //  The number in the left brackets is the x and the right is the y,
+        //  so like "daGrid[0][3]" is all the way to the left, but three squares down.
         daGrid[0][0] = new BlankGridSpace((ImageView) findViewById(R.id.grid0x0), xLevelMove);
         daGrid[0][1] = new BlankGridSpace((ImageView) findViewById(R.id.grid0x1), xLevelMove);
         daGrid[0][2] = new BlankGridSpace((ImageView) findViewById(R.id.grid0x2), xLevelMove);
@@ -250,13 +149,17 @@ public class LevelOneActivity extends AppCompatActivity
         daGrid[12][1] = new BlankGridSpace((ImageView) findViewById(R.id.grid12x1), xLevelMove);
         daGrid[12][2] = new BlankGridSpace((ImageView) findViewById(R.id.grid12x2), xLevelMove);
         daGrid[12][3] = new BlankGridSpace((ImageView) findViewById(R.id.grid12x3), xLevelMove);
-
-        //  Idk if the cast from View to ImageView will work, let's see....
-        //  daGrid[12][4] is the one with the SquareObstacle:
+        //  daGrid[12][4] is a SquareObstacle: 
         daGrid[12][4] = new SquareObstacle((ImageView) findViewById(R.id.grid12x4), width, height, bobImage, xLevelMove, bob);
-        //  This works, so why doesn't an actual Square?
-        //daGrid[12][4] = new BlankGridSpace((ImageView) findViewById(R.id.grid12x4), xLevelMove);
         daGrid[12][5] = new BlankGridSpace((ImageView) findViewById(R.id.grid12x5), xLevelMove);
+
+        daGrid[13][0] = new BlankGridSpace((ImageView) findViewById(R.id.grid13x0), xLevelMove);
+        daGrid[13][1] = new BlankGridSpace((ImageView) findViewById(R.id.grid13x1), xLevelMove);
+        daGrid[13][2] = new BlankGridSpace((ImageView) findViewById(R.id.grid13x2), xLevelMove);
+        //  Another SquareObstacle:
+        daGrid[13][3] = new SquareObstacle((ImageView) findViewById(R.id.grid13x3), width, height, bobImage, xLevelMove, bob);
+        daGrid[13][4] = new BlankGridSpace((ImageView) findViewById(R.id.grid13x4), xLevelMove);
+        daGrid[13][5] = new BlankGridSpace((ImageView) findViewById(R.id.grid13x5), xLevelMove);
 
 
 
@@ -265,32 +168,15 @@ public class LevelOneActivity extends AppCompatActivity
         //  or off of the screen! (stuff to the right that will gradually move left
         //  onto the screen)
         for (int index=0; index<daGrid.length; index++)
-        //for (int index=0; index<1; index++)
         {
             for (int index2=0; index2<daGrid[index].length; index2++)
             {
-                //daGrid[index][index2].getLayoutParams().height = height / 7;
                 daGrid[index][index2].setImageHeight(height/7);
-                //daGrid[index][index2].getLayoutParams().width = width / 12;
                 daGrid[index][index2].setImageWidth(width/12);
-                //daGrid[index][index2].setX(index*width/12);
                 daGrid[index][index2].setImageX(index*width/12);
-                //daGrid[index][index2].setY((height / 14) + (index2 * height / 7));
                 daGrid[index][index2].setImageY((height / 14) + (index2*height/7));
             }
         }
-
-
-        /*bob = findViewById(R.id.bob);
-        bob.setX(0);
-        bob.setY(11*height/14);
-        bob.getLayoutParams().height = height/7;
-        bob.getLayoutParams().width = width/12;*/
-
-
-
-        //  Only Square for now
-        //onlySquare = new SquareObstacle(daGrid[12][4], width, height, bobImage, xLevelMove, bob, daGrid);
 
         //  Runs the timer once every 0.35 of a second or something, idk, 500 would be once every 0.5 s
         //  but the first timer call isn't until after a second (1,000) of delay.
@@ -310,78 +196,16 @@ public class LevelOneActivity extends AppCompatActivity
     }
 
 
-
-
     public void levelMoveStuff()
-    {
-        // Square Obstacle decides whether to move grid, or stop it bec Bob collided w something!
-        /*for (int index=0; index<daGrid.length; index++)
-        {
-            for (int index2=0; index2<daGrid[index].length; index2++)
-            {
-                daGrid[index][index2].setX(daGrid[index][index2].getX() - xLevelMove);
-            }
-        }*/
-
-        //  Bob needs to redraw even though staying still to be over top of the grid backgrounds.
-        //bob.setX(bob.getX());
-        //  Never mind, that doesn't work at all! Apparently, the image at the bottom of the Component Tree
-        //  is the foreground/forwardmost, so moved bob to bottom, and now he appears on top of grid.
-
-
-
-        /*if (jumpingNow == true)
-        {
-            //bobY = bobY - height/98;
-            bob.setY(bob.getY()-yBobJump);
-            //if (bobY > 151)
-            //if (bobY < 3*height/7)
-            //if (bobY <= 7*height/14)
-            if (bob.getY() <= 6*height/14)
-            {
-                jumpingNow = false;
-                fallingNow = true;
-            }
-        }
-        if (fallingNow == true)
-        {
-            //bobY = bobY + height/98;
-            bob.setY(bob.getY()+yBobJump);
-            //if (bobY > 5*height/7)
-            if (bob.getY() >= 11*height/14)
-            {
-                fallingNow = false;
-            }
-        }*/
-
-
-        //  Switching with the Square:
-        //bob.midJumpStuff();
-
-        //  Probobly everything will stay here.
-        //onlySquare.checkCollision();
-
-
-
-    moveDaGrid();
-
-    }
-
-    //  Unlike the grid placement double for-loop, this one moves the grid.
-    private void moveDaGrid()
     {
         boolean gridShouldMove = true;
         for (int index=0; index<daGrid.length; index++)
         {
             for (int index2=0; index2<daGrid[index].length; index2++)
             {
-                //daGrid[index][index2].setX(daGrid[index][index2].getX() - xMoveSpeedScreen);
-                //daGrid[index][index2].setImageX(daGrid[index][index2].getImageX() - xLevelMove);
-
-                //  NEW NEW NEW NEW NEW ENW
-                //  Whoops, this is probably getting reset to true after the Square the next BlandSpace.
                 gridShouldMove = daGrid[index][index2].checkCollision();
-                //  Hopefully this will exit this loop and immediately go to the if-statement below.
+                //  If Bob is about to crash into even a single square, exit the loop immediately so that this
+                //  doesn't get reset to true!
                 if (gridShouldMove == false)
                 {
                     break;
@@ -402,15 +226,9 @@ public class LevelOneActivity extends AppCompatActivity
         }
     }
 
+
     public boolean onTouchEvent(MotionEvent event)
     {
-
-        /*//if (bobY > 151)
-        if (jumpingNow==false && fallingNow==false)
-        {
-            jumpingNow = true;
-        }*/
-
         //  Checks to see if Bob can jump or if there's like a SquareObstacle or something in the way.
         bob.startJumpMaybe();
 
